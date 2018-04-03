d50da19f86d71d1b623627a086ea185bfcf49f69
huoda <1997huoda@gmail.com>
2018-04-03 21:46:58 +0800

    我不知道对不对

---
+++ List/app/src/main/java/com/glandroid/list/MainActivity.java
@@ -0,0 +1,30 @@
+package com.glandroid.list;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout ll_container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ll_container = (LinearLayout) findViewById(R.id.ll_container);
+    }
+
+    public void show(View view) {
+        for (int i = 0; i < 1000000; i++) {
+            TextView tv = new TextView(this);
+            tv.setText("我是文本：" + i);
+            tv.setTextColor(Color.RED);
+            tv.setTextSize(20);
+            ll_container.addView(tv);
+        }
+    }
+}
